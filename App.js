bb483a9cb6c61026d19a862c11da52f10d2124fb
singhlaaman <singhlaaman@gmail.com>
2019-04-13 22:20:43 +0100

    1 file modified

--- App.js
+++ App.js
@@ -8,14 +8,14 @@ function App() {
     return (
         <div className = "container">
           <Row>
-            <Col xs="4" sm="2" className = "leftSide">.col-6 .col-sm-4</Col>
+            <Col xs="4" sm="2" className = "leftSide"><img src= "https://www.businessinsider.com.au/nfl-person-of-interest-deflategate-2015-1"/></Col>
             <Col xs="8" sm="6" className = "middle">
               <div>
                 <FormGroup>
-                  <Label for="exampleText">Text Area</Label>
-                  <Input type="textarea" name="text" id="exampleText" />
+                  <Label for="exampleText">Post Your Status Here:</Label>
+                  <Input type="textarea" name="text" id="exampleText" placeholder="Your Status Here..."/>
                 </FormGroup>
-                <Button className="button">Submit</Button>
+                <Button className="button">Post</Button>
               </div>
               <div>
                 <Example />
