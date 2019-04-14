220ea1ff623fe242d702aeb1aa02f9313868c072
singhlaaman <singhlaaman@gmail.com>
2019-04-14 12:23:20 +0100

    3 files modified

--- App.js
+++ App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react'
 import Example from './Example'
-import { Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Row, Col, Container, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 import './App.css'
 
 function App() {
     
     return (
-        <div className = "container">
+        <div>
+        <Container>
           <Row>
-            <Col xs="4" sm="2" className = "leftSide">Your Profile Comes Here..</Col>
+            <Col xs="4" sm="2" className = "leftSide"></Col>
             <Col xs="8" sm="6" className = "middle">
               <div>
                 <FormGroup>
-                  <Label for="exampleText">Post Your Status Here:</Label>
-                  <Input type="textarea" name="text" id="exampleText" placeholder="Your Status Here..."/>
-                </FormGroup>
-                <Button className="button">Post</Button>
+                  <h1>You have been making negative posts lately. Feeling down? </h1><br /> <h3>Here's some resources local to you :-</h3>
+                  </FormGroup>
+                
               </div>
               <div>
                 <Example />
               </div>  
               </Col>
-            <Col xs="4" sm="2" className = "leftSide">You can make friends with these people..</Col>
+            <Col xs="4" sm="2" className = "leftSide"></Col>
           </Row>
+          </Container>
         </div>
     )
 }
--- Example.js
+++ Example.js
@@ -6,34 +6,14 @@ const Example = (props) => {
   return (
      <div>
       <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }} className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button>Button</Button>
-      </Card>
-      <Card body inverse color="primary" className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="success" className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="info" className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-      <Card body inverse color="warning" className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
+        <CardTitle><h2>Lifeline Helpline NI</h2></CardTitle>
+        <CardText>Lifeline is the Northern Ireland crisis response helpline service for people who are experiencing distress or despair. No matter what your age or where you live in Northern Ireland, if you are or someone you know is in distress or despair, Lifeline is here to help.</CardText>
+        <Button>Call</Button>
       </Card>
       <Card body inverse color="danger" className = "button">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
+        <CardTitle><h2>Helplines Network NI</h2></CardTitle>
+        <CardText>Helplines Network NI is a membership–led organisation consisting of over 30 different helplines operating across Northern Ireland. The helplines provide a variety of vital support services including, information, advice, counselling, a listening ear and be-friending, and offer immediate support to those with a wide-range of health and wellbeing needs, often for those who are the most vulnerable in our community. Many of the services take place over the phone, but some can help via email, text and in some cases with live web-chat. </CardText>
+        <Button style={{ backgroundColor: '#333', borderColor: '#333' }}>Call</Button>
       </Card>
     </div>
   );
